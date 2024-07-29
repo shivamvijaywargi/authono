de5b04da81f829c245baf32f10f138e0165d56d6
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2024-07-29 21:20:13 +0530

    perf: update logger config to not run a setTimeout everyday⚡️

--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -1,35 +1,18 @@
-import path from "path";
-import fs from "fs";
-
 import pino from "pino";
 
 import { REDACTED_FIELDS } from "../utils/constants";
-
-function getLogFilename(type = "combined") {
-  const now = new Date();
-  const year = now.getFullYear();
-  const month = String(now.getMonth() + 1).padStart(2, "0");
-  const day = String(now.getDate()).padStart(2, "0");
-  const logDir = path.join(process.cwd(), "logs");
-
-  // Ensure the logs directory exists
-  if (!fs.existsSync(logDir)) {
-    fs.mkdirSync(logDir, { recursive: true });
-  }
-
-  return path.join(logDir, `${type}-${year}-${month}-${day}.log`);
-}
+import env from "../env";
 
 const transport = pino.transport({
   targets: [
     {
       target: "pino/file",
-      options: { destination: getLogFilename("combined"), mkdir: true },
+      options: { destination: "logs/combined.log", mkdir: true },
     },
     {
       level: "error",
       target: "pino/file",
-      options: { destination: getLogFilename("error"), mkdir: true },
+      options: { destination: "logs/error.log", mkdir: true },
     },
     {
       target: "pino-pretty",
@@ -43,7 +26,7 @@ const transport = pino.transport({
 export const logger = pino(
   {
     name: "authono",
-    level: "debug",
+    level: env.LOG_LEVEL || "debug",
     formatters: {
       bindings: (bindings) => {
         return {
@@ -70,44 +53,3 @@ process.on("uncaughtException", (error) => {
 process.on("unhandledRejection", (reason, promise) => {
   logger.error({ reason, promise }, "Unhandled Rejection");
 });
-
-// Function to update log files
-function updateLogFiles() {
-  const newCombinedFile = getLogFilename("combined");
-  const newErrorFile = getLogFilename("error");
-
-  transport.targets.forEach((target: any) => {
-    if (target.target === "pino/file") {
-      target.options.destination = target.options.destination.includes("error")
-        ? newErrorFile
-        : newCombinedFile;
-    }
-  });
-
-  logger.info(
-    `Log files rotated: Combined - ${newCombinedFile}, Error - ${newErrorFile}`
-  );
-}
-
-// Function to schedule next midnight
-function scheduleNextMidnight() {
-  const now = new Date();
-  const night = new Date(
-    now.getFullYear(),
-    now.getMonth(),
-    now.getDate() + 1, // the next day
-    0, // at 00:00:00 hours
-    0,
-    0
-  );
-  const msToMidnight = night.getTime() - now.getTime();
-
-  setTimeout(() => {
-    updateLogFiles();
-    scheduleNextMidnight(); // Schedule the next one
-  }, msToMidnight);
-}
-
-// Initial update and scheduling
-// updateLogFiles();
-scheduleNextMidnight();
